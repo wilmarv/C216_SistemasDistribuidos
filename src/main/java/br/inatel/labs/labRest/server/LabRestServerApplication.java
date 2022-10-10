6264ee9c094f506bdeec2f71255a1d7123f0405a
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-10-10 13:01:49 -0300

    Projeto inicializado

---
+++ src/main/java/br/inatel/labs/labRest/server/LabRestServerApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labRest.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabRestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/inatel/labs/labRest/server/LabRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.labs.labRest.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabRestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
