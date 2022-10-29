b446ee0421c627e81c655828ed311dbe4e85990c
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-10-29 06:02:43 -0300

    Lab JPA Part1

---
+++ lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Produto.java
@@ -0,0 +1,71 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@NotBlank
+	@Size(max = 100)
+	private String descricao;
+	
+	@ManyToMany
+	private List<Fornecedor> listaFornecedor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public List<Fornecedor> getListaFornecedor() {
+		return listaFornecedor;
+	}
+
+	public void setListaFornecedor(List<Fornecedor> listaFornecedor) {
+		this.listaFornecedor = listaFornecedor;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
