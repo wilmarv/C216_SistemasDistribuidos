838799098abde0f22afb649d38bce7d9259aa510
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-10-29 15:11:34 -0300

    Lab Jpa part4

---
+++ lab_jpa/src/test/java/br/inatel/labs/labjpa/service/ProdutoServiceTest.java
@@ -0,0 +1,34 @@
+package br.inatel.labs.labjpa.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.inatel.labs.labjpa.entity.Produto;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ProdutoServiceTest {
+
+	@Autowired
+	private ProdutoService service;
+
+	@Test
+	void dadoIdValido_quandoInvocaBuscarPeloId_entaoProdutoRetornadoEhValido() {
+
+		Long idValido = 1L;
+
+		Produto produtoEncontrado = service.buscarPeloId(idValido);
+
+		assertFalse(produtoEncontrado == null);
+		assertFalse(produtoEncontrado.getDescricao().isBlank());
+
+		assertEquals(produtoEncontrado.getId(), idValido);
+
+	}
+
+}
