100bb5521e050d278e40b98b7e95fad9097f5cdf
wilmar vitor fonseca <wilmarfonseca@gec.inatel.br>
2022-11-26 14:42:02 -0300

    Inicializando LabMQQT

--- lab_jpa/src/test/resources/data.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM Produto;
-
-INSERT INTO Produto (id,descricao) VALUES(1,'Serrote');
-INSERT INTO Produto (id,descricao) VALUES(2,'Martelo');
-INSERT INTO Produto (id,descricao) VALUES(3,'Prego');
